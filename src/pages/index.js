288b560bf090b181d4d26f5149aa23a1bc81dc6a
JulienAssouline <julien1993@hotmail.ca>
2019-09-13 16:01:57 -0400

    new cv

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,36 +1,15 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import "../styling/styles.css"
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
-import Background from "../components/Background"
 
-import ImageComp from "../components/ImageComp"
-import chord_diagram from "../images/Flourish_Chord_diagram.png"
-import Flourish_Parliament from "../images/Flourish_Parliament.png"
-import FiveThirtyEight from "../images/538.png"
-import Baseball_display from "../images/Baseball_display.png"
-import Grant_contest from "../images/Grant_contest.png"
-import North_Korea from "../images/North_Korea_display.png"
-import Oscors_race from "../images/Oscors_race_display.png"
-import sunshine_list from "../images/sunshine_list.png"
-import display_friends from "../images/display_friends.png"
-import video_games_movies from "../images/video_games_movies.png"
-import r10 from "../images/r10.png"
-import bazaar from "../images/bazaar.png"
-import space_explorer from "../images/space_explorer.png"
-import dogstagram from "../images/dogstagram.png"
-import hiRED from "../images/hiRED.png"
-
-import Logo from "../components/Logo"
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faTwitter, faLinkedin, faGithub} from '@fortawesome/fontawesome-free-brands';
-
-
-
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+  faTwitter,
+  faLinkedin,
+  faGithub,
+} from "@fortawesome/fontawesome-free-brands"
 
 const skills = {
   HTML: "HTML",
@@ -54,38 +33,47 @@ const skills = {
   Three: "Three.js",
 }
 
-
-
 const IndexPage = () => (
   <Layout>
-    <div className = "home-container">
-    <Background />
-      <div className = "hero-text-container">
-      <div className="name">
-        <svg height="100" width="1000" style = {{strokeWidth: 2}} stroke="#333"  className="text-line">
-            <text className = "name-text" x="0" y="80" fill="none" >Hey, I'm Julien Assouline</text>
+    <div className="home-container">
+      <div className="hero-text-container">
+        <div className="name">
+          <svg
+            height="100"
+            width="1000"
+            style={{ strokeWidth: 2 }}
+            stroke="#333"
+            className="text-line"
+          >
+            <text className="name-text" x="0" y="80" fill="none">
+              Hey, I'm Julien Assouline
+            </text>
           </svg>
-      </div>
-      <p className = "title-description">I'm a Full Stack Developer passionate about building applications, tools, data visualizations and coding, in general, to improve peoples lives and make the world a better place.</p>
-      <div className = "icons-container">
-            <a href="mailto:julien1993@hotmail.ca"> <FontAwesomeIcon
-                    icon={faEnvelope}
-                    className = "icon fa-5x"
-                    /> </a>
-           <a href="https://github.com/JulienAssouline"> <FontAwesomeIcon
-                      icon={faGithub}
-                      className = "icon fa-5x"
-                      /> </a>
-          <a href="https://www.linkedin.com/in/julienassouline/"> <FontAwesomeIcon
-                    icon={faLinkedin}
-                    className = "icon fa-5x"
-                    /> </a>
+        </div>
+        <p className="title-description">
+          I'm a Full Stack Developer passionate about building applications,
+          tools, data visualizations and coding, in general, to improve peoples
+          lives and make the world a better place.
+        </p>
+        <div className="icons-container">
+          <a href="mailto:julien1993@hotmail.ca">
+            {" "}
+            <FontAwesomeIcon icon={faEnvelope} className="icon fa-5x" />{" "}
+          </a>
+          <a href="https://github.com/JulienAssouline">
+            {" "}
+            <FontAwesomeIcon icon={faGithub} className="icon fa-5x" />{" "}
+          </a>
+          <a href="https://www.linkedin.com/in/julienassouline/">
+            {" "}
+            <FontAwesomeIcon icon={faLinkedin} className="icon fa-5x" />{" "}
+          </a>
 
-           <a href="https://twitter.com/JulienAssouline"> <FontAwesomeIcon
-                      icon={faTwitter}
-                      className = "icon fa-5x"
-                      /> </a>
-          </div>
+          <a href="https://twitter.com/JulienAssouline">
+            {" "}
+            <FontAwesomeIcon icon={faTwitter} className="icon fa-5x" />{" "}
+          </a>
+        </div>
       </div>
     </div>
   </Layout>
